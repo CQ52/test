fb4e622ab5c895d6f6ade77b00102c12f79b52c5
CQ52 <CQ52@CQ_HBK@163.com>
2020-09-21 22:59:49 +0800

    添加了like模块

--- git_test/git_test/settings.py
+++ git_test/git_test/settings.py
@@ -68,8 +68,11 @@ WSGI_APPLICATION = 'git_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'CQ',
+		'HOST':'121.42.229.231',
+		'PASSWORD':'HBK190601hbk*2',
+		'USER':'root'
     }
 }
 
